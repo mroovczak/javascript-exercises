b58c95567043047e45ea6e251605bc996ad08419
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 04:37:48 +0200

    fix revers single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    reversed_text = "";
+    for (i=0; i<text.length; i++){
+        reversed_text += text[text.length-i-1]; 
+    }
+    return reversed_text;
 };
 
 // Do not edit below this line
