73fa39d649d0ae001531795bbef790b3da37449e
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 05:53:16 +0200

    add checking for lower number function to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,16 @@ const sumAll = function(number1, number2) {
     //NaN Not a Number
     if (isNaN(arguments[0]) || isNaN(arguments[1])) return "ERROR";
     if (isNaN(arguments[0]) < 0 || (arguments[1]) <0) return "ERROR";
-    for(let i=number1; i<=number2; i++){
+    //makes sure loops go from lower to higher number
+    let lower,higher;
+    if (number1<number2){
+        lower = number1;
+        higher = number2;
+    }else {
+        lower = number2;
+        higher = number1;
+    }
+    for(let i= lower; i<=higher; i++){
         sum += i;
     }
     return sum;
