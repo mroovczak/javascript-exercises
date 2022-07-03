f049bf3e18f2cefd3049b999e1c47d7afed49bb3
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 05:54:38 +0200

    fix sumAll for sub 0 values

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,7 +2,7 @@ const sumAll = function(number1, number2) {
     let sum = 0;
     //NaN Not a Number
     if (isNaN(arguments[0]) || isNaN(arguments[1])) return "ERROR";
-    if (isNaN(arguments[0]) < 0 || (arguments[1]) <0) return "ERROR";
+    if (arguments[0] < 0 || arguments[1] <0) return "ERROR";
     //makes sure loops go from lower to higher number
     let lower,higher;
     if (number1<number2){
