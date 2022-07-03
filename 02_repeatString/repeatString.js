ad0fef5d5f6dd505954aed25ff1571e7900ccac7
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 04:19:43 +0200

    finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    text_returned="";
+    if (times<0) return "ERROR";
+    for (i=0; i<times; i++) {
+        text_returned +=text;
+    }
+    return text_returned;
 };
 
 // Do not edit below this line
