63a87ce59a56758927f4d75ef0306fd88e994acd
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 16:08:41 +0200

    add palindrome func

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const reverseString = require('../03_reverseString/reverseString')
+const palindromes = function (text) {
+    //used regular expresion to replace all special chars at once 
+    //added /g paramter regexp must be global!
+    striped = text.replaceAll( /[&\/\\#,\ \s +()$~%.'":*?<>{!}]/g,"");
+    striped = striped.toLowerCase();
+    reversed = reverseString(striped);
+    return striped == reversed ? true : false;
 };
 
 // Do not edit below this line
