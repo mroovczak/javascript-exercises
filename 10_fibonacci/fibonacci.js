9ff6eeb55537890847110ffcc215112bc780cbd2
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 16:21:24 +0200

    add fibonaci func

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+let fib=[1,1]
+    for(let i = 2; i<num; i++){
+        fib[i] = fib[i-1]+fib[i-2];
+    }
+    return fib[num-1];
 };
 
 // Do not edit below this line
