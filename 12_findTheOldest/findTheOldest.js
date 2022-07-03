fc19c94581b9e8c924253fe255d1d65e33a37bda
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 16:59:00 +0200

    add findTheOdlest func

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPersonAge = 0;
+    let oldestPerson;
+    const date = new Date();
+    let yearTo;
+    people.forEach(person => {
+        person["yearOfDeath"] === undefined ? yearTo = date.getFullYear() : yearTo = person["yearOfDeath"];
+        let age = yearTo - person["yearOfBirth"];
+        if (age > oldestPersonAge) {
+            oldestPersonAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
