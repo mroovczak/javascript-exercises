7896cb0a552288765eb37aaf8d6b08390d85adc1
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 17:44:00 +0200

    add cipher using codePointAt (to unicode)

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,15 @@
-const caesar = function() {
-
+const caesar = function(text, shift) {
+    let textUnicode=[];
+    let ciphered= "";
+    for (let index = 0; index < text.length; index++) {
+        if (text[index].codePointAt() < 123 && text[index].codePointAt() >64) { 
+            textUnicode[index] = text[index].codePointAt() + shift;
+        } else {
+            textUnicode[index] = text[index].codePointAt();
+        }
+    }
+    ciphered = textUnicode.map(x => String.fromCodePoint(x)).join("");
+    return ciphered;
 };
 
 // Do not edit below this line
