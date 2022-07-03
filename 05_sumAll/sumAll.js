140b67168564eef5804587d1d26ed30b0da060bd
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 05:43:16 +0200

    add sum all works 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let sum = 0;
+    //NaN Not a Number
+    if (isNaN(arguments[0]) || isNaN(arguments[1])) return "ERROR";
+    if (isNaN(arguments[0]) < 0 || (arguments[1]) <0) return "ERROR";
+    for(let i=number1; i<=number2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
