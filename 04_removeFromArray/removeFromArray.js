adb13d0ab03334c3dbcebf14a90a39b51f405330
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 05:27:31 +0200

    add remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArrayFilter = function(item, arr){
+    if(arr.includes(item) == false) return true;
+};
+const removeFromArray = function(arr) {
+    let result;
+    if (arguments.length>1) {
+        //arguemnts assigned to an array (arguments[] isnt really an array)
+        let args = Array.prototype.slice.call(arguments);
+        result = arr.filter(item => removeFromArrayFilter(item,args))
+    }
+    return result;
 };
 
 // Do not edit below this line
