ea3f268ea27b74d2a6aafb875fcc7b6bf063fad5
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 15:06:45 +0200

    add tempConversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celcius = Math.round((fahrenheit-32)*(5/9) *10) /10;
+  return celcius;
 };
-
-const ctof = function() {
-
+const ctof = function(celcius) {
+  fahrenheit = Math.round((celcius * (9/5) + 32) *10) /10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
