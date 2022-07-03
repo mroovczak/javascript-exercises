4cb3433f598d2b037447c516ec9a501427b5368a
Bartek Kowalczyk <mrowczak1994@gmail.com>
2022-07-03 20:07:25 +0200

    add new function in ceasar.js that maps characters

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,14 +1,25 @@
-const caesar = function(text, shift) {
-    let textUnicode=[];
-    let ciphered= "";
-    for (let index = 0; index < text.length; index++) {
-        if (text[index].codePointAt() < 123 && text[index].codePointAt() >64) { 
-            textUnicode[index] = text[index].codePointAt() + shift;
-        } else {
-            textUnicode[index] = text[index].codePointAt();
-        }
+const shiftAmount = function(char, amount){
+    const alphaUpper = Array.from(Array(26)).map((e, i) => i + 65);
+    const alphaLower = Array.from(Array(26)).map((e, i) => i + 97);
+    const lettersUpper = alphaUpper.map((x) => String.fromCharCode(x));
+    const lettersLower = alphaLower.map((x) => String.fromCharCode(x));
+    let index =-1;
+    index = lettersUpper.findIndex(element => element === char);
+    if(index != -1){
+        return lettersUpper[(index+amount)<26 ? (index+amount) : (index+amount) - 26];
+    } 
+    index = lettersLower.findIndex(element => element === char);
+    if(index != -1){
+        return lettersLower[(index+amount)<26 ? (index+amount) :(index+amount) - 26];
+    } else {
+        return char;
     }
-    ciphered = textUnicode.map(x => String.fromCodePoint(x)).join("");
+};
+const caesar = function(text, shift) {
+
+    let ciphered= "";
+    let textUnicode =  text.split("").map(char => shiftAmount(char, shift));
+    ciphered = textUnicode.join("");
     return ciphered;
 };
 
